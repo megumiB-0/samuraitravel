dcacd3cf3a8463964115ab015f5914ca4709caeb
megumiB-0 <megumi.b.design@gmail.com>
2025-11-23 15:54:38 +0900

    会員用の会員編集ページ作成

--- src/main/java/com/example/samuraitravel/controller/UserController.java
+++ src/main/java/com/example/samuraitravel/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.samuraitravel.entity.User;
+import com.example.samuraitravel.form.UserEditForm;
 import com.example.samuraitravel.security.UserDetailsImpl;
 
 @Controller
@@ -17,5 +18,14 @@ public class UserController {
 		User user = userDetailsImpl.getUser();	
 		model.addAttribute("user", user);
 		return "user/index";
+	}
+	
+	@GetMapping("/edit")
+	public String edit(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,Model model) {
+		User user = userDetailsImpl.getUser();
+		UserEditForm userEditForm = new UserEditForm(user.getName(), user.getFurigana(), user.getPostalCode(), user.getAddress(), user.getPhoneNumber(), user.getEmail());
+	
+		model.addAttribute("userEditForm",userEditForm);
+		return "user/edit";
 	}
 }
